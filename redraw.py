a53e3c7359b41bd15f8882039eac1a012e91ea5c
Shimadaaaaa <48462439+Shimadaaaaa@users.noreply.github.com>
2020-11-14 15:55:58 +0800

    version 1.0

---
+++ redraw.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Author: hzb
+# @Date:   2020-11-12 00:08:25
+# @Last Modified by:   hzb
+# @Last Modified time: 2020-11-12 00:45:57
+import os
+import cv2
+import numpy as np
+
+def redraw(img, label_path):
+    try:
+        with open(label_path, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.replace('\n', '')
+                box = line.split()
+                box[:4] = list(map(int, box[:4]))
+                img = cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0, 0, 255), 2)
+                img = cv2.putText(img, box[4], (box[0], box[1]-3),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        return img
+    except:
+        return img
+    
+    
+if __name__ == "__main__": 
+    img = redraw('/home/jonthan/下载/img/000001.jpg', '/home/jonthan/下载/txt/000001.txt')
+    cv2.imshow('show', img)
+    cv2.waitKey(0)
